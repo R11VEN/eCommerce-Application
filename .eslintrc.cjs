283e6c53f745799e6f39cd86884c4b00f05d3f21
Anton <72494592+AntonFartovii@users.noreply.github.com>
2023-08-04 19:26:57 +0300

    feat: set up eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,12 @@
 module.exports = {
+  parserOptions: {
+    "ecmaVersion": 2020,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "project": "./tsconfig.json"
+  },
   root: true,
   env: { browser: true, es2020: true },
   extends: [
@@ -8,11 +16,63 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react',
+    'react-refresh',
+    '@typescript-eslint',
+    'simple-import-sort'
+  ],
   rules: {
+    'simple-import-sort/exports': 'warn',
+    'simple-import-sort/imports': 'warn',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        'extensions': ['.ts', '.tsx']
+      },
+    ],
+    "react/function-component-definition": [
+      2,
+      {
+        'namedComponents': 'arrow-function',
+        'unnamedComponents': 'arrow-function'
+      }
+    ],
+    "@typescript-eslint/ban-ts-comment": "warn",
+    "@typescript-eslint/no-unsafe-return": "warn",
+    "@typescript-eslint/no-var-requires": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "import/extensions": "off",
+    "no-unused-vars": "off",
+    "no-console": "warn",
+    "func-names": "off",
+    "no-process-exit": "off",
+    "object-shorthand": "off",
+    "class-methods-use-this": "off",
+    "no-duplicate-imports": "error"
   },
+  overrides: [
+    {
+      "files": ["*.js"],
+      "rules": {
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/no-unused-vars": "off"
+      }
+    },
+    {
+      "files": ["*.ts", "*.tsx"],
+      "rules": {
+        "no-unused-vars": "off"
+      }
+    }
+  ]
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+import { useState } from 'react'
+
+import viteLogo from '/vite.svg'
+
+import reactLogo from './assets/react.svg'
+
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
