bd917525b46f2483495d9783d6c305b41db61b1b
Max Komarov <b-raven@yandex.ru>
2023-08-07 15:17:45 +0300

    feat: install identity-obj-proxy, configure moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   "collectCoverage": true,
-  "coverageReporters": ["html"]
+  "coverageReporters": ["html"],
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': `${__dirname}/src/assets/css/__mock__/fileMock.tsx`,
+    ".(css|sass|scss)$": "identity-obj-proxy",
+  },
 };
