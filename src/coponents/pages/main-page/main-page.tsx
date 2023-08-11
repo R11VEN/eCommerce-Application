fc6d4252ba308737281fd5cbaf8ed85278311041
Ruslanbek <you@example.com>
2023-08-11 12:01:47 +0500

    feat: add basic components

---
+++ src/coponents/pages/main-page/main-page.tsx
@@ -0,0 +1,5 @@
+const MainPage = () => {
+  return <div className="main">This is Main Page</div>;
+};
+
+export default MainPage;
---
+++ src/coponents/pages/pages.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MainPage from './main-page/main-page';
+import Header from '../header/header';
+import Footer from '../footer/footer';
+
+const Pages = () => {
+  return (
+    <div className="pages">
+      <Header />
+      <MainPage />
+      <Footer />
+    </div>
+  );
+};
+export default Pages;
