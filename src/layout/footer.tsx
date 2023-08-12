ef43fcd1992d5df27bbf74fa9ede8c626c9e770c
Max Komarov <b-raven@yandex.ru>
2023-08-12 14:03:55 +0300

    feat: add bace styles

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,4 +1,10 @@
 const Footer = () => {
-  return <div className="footer">This is Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <h2 className="footer-logo">High Voltage</h2>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
