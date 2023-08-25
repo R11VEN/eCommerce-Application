7e65818e1a9e312e4a7d3dd5fb3b2ff88b974225
Ruslanbek Normurodov <aksirus89@gmail.com>
2023-08-25 19:49:00 +0500

    Fix import bug

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import { useSelector } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import { AuthState, RootState } from './interfaces/state.interface.ts';
-import Layout from './layout/Layout.tsx';
-import { CatalogPage } from './pages/catalog-page.tsx';
+import Layout from './layout/Layout.tsx' 
 
 const App = () => {
   const auth: AuthState = useSelector((state: RootState) => state.auth);
