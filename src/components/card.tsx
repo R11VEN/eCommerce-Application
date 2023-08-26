0524e7f23d4dd9443d0053856b7a375bd191d9bb
Ruslanbek <you@example.com>
2023-08-26 17:24:03 +0500

    feat: add DetailedProductPage

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,19 +1,19 @@
-import { useNavigate } from 'react-router-dom';
-import classes from '../css/ui.module.css';
-import { AUTH_ROUTE } from '../constants/pages';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
-export const Card = ({ title, price }: { title: string; price: number }) => {
-  const navigate = useNavigate();
-  function cardClckHandler() {
-    navigate(AUTH_ROUTE);
-  }
+import { PRODUCT_ROUTE } from '../constants/pages';
+import { DetailedProductPage } from '../pages/detailedProductPage';
+import classes from '../css/ui.module.css';
+
+export const Card = ({ title, price, id }: { title: string; price: number; id: number }) => {
   return (
-    <div className={classes.card} onClick={cardClckHandler}>
-      <img className={classes.card__img} src="" alt="" />
-      <div className="description">
-        <h2 className="card__title">{title}</h2>
-        <p className="card__price">{price}</p>
+    <NavLink to={`${PRODUCT_ROUTE}/${id}`}>
+      <div className={classes.card}>
+        <img className={classes.card__img} src="" alt="" />
+        <div className="description">
+          <h2 className="card__title">{title}</h2>
+          <p className="card__price">{price}</p>
+        </div>
       </div>
-    </div>
+    </NavLink>
   );
 };
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
-import { AUTH_ROUTE, MAIN_ROUTE, PAGE_404, REG_ROUTE } from './constants/pages.ts';
+import { AUTH_ROUTE, MAIN_ROUTE, PAGE_404, PRODUCT_ROUTE, REG_ROUTE } from './constants/pages.ts';
 import { RouteInterface } from './interfaces/route.interface.ts';
 import AuthPage from './pages/auth-page.tsx';
+import { DetailedProductPage } from './pages/detailedProductPage.tsx';
 import MainPage from './pages/main-page.tsx';
 import Page404 from './pages/page404.tsx';
 import RegPage from './pages/reg-page.tsx';
@@ -49,5 +50,10 @@ export const routesAll: RouteInterface[] = [
     name: 'Page 404',
     path: PAGE_404,
     Component: ({ showPageName }) => <Page404 showName={showPageName} />,
+  },
+  {
+    name: 'Detailed Product Page',
+    path: PRODUCT_ROUTE + '/:id',
+    Component: ({ showPageName }) => <DetailedProductPage showName={showPageName} />,
   },
 ];
