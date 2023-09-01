dc6ede2fe8afc4dda91e9f5356e7f451abb36344
Max Komarov <b-raven@yandex.ru>
2023-09-01 19:18:59 +0300

    feat: add new endpiont, discounts

--- src/api/productsGet.tsx
+++ src/api/productsGet.tsx
@@ -1,11 +1,17 @@
-import { ApiRoot, ClientResponse, ProductPagedQueryResponse } from '@commercetools/platform-sdk';
+import {
+  ApiRoot,
+  ClientResponse,
+  ProductProjectionPagedQueryResponse,
+} from '@commercetools/platform-sdk';
 
 import { apiRootSPA, projectKey } from './BuildClientSPA';
 
 interface IProductRepository {
   apiRoot: ApiRoot;
   projectKey: string;
-  getProducts(): Promise<ClientResponse<ProductPagedQueryResponse> | undefined>;
+  getProducts(
+    offset: number
+  ): Promise<ClientResponse<ProductProjectionPagedQueryResponse> | undefined>;
 }
 
 class Products implements IProductRepository {
@@ -16,12 +22,18 @@ class Products implements IProductRepository {
     this.projectKey = projectKey;
   }
 
-  async getProducts() {
+  async getProducts(offset: number) {
     try {
       const products = await this.apiRoot
         .withProjectKey({ projectKey: this.projectKey })
-        .products()
-        .get()
+        .productProjections()
+        .get({
+          queryArgs: {
+            limit: 8,
+            offset: offset,
+            sort: 'name.ru-BY',
+          },
+        })
         .execute();
 
       return products;
--- src/pages/catalog-page.tsx
+++ src/pages/catalog-page.tsx
@@ -1,51 +1,88 @@
-import { ProductPagedQueryResponse } from '@commercetools/platform-sdk';
+import { ProductProjectionPagedQueryResponse } from '@commercetools/platform-sdk';
 import { useCallback, useEffect, useState } from 'react';
 
 import Products from '../api/productsGet.tsx';
-import { Button } from '../components/button.tsx';
 import { Card } from '../components/card.tsx';
 import classes from '../css/ui.module.css';
 import { PageProps } from '../interfaces/page.interface.ts';
 
 export const CatalogPage = ({ showName }: PageProps): JSX.Element => {
-  const [products, setProducts] = useState<ProductPagedQueryResponse>();
+  useEffect(() => {
+    showName && showName('Catalog');
+  }, []);
+  const [productsOffset, setPagin] = useState(0);
+  const [products, setProducts] = useState<ProductProjectionPagedQueryResponse>();
+  const [page, setPage] = useState(1);
+
   const getProducts = useCallback(async () => {
     const product = new Products();
-    await product.getProducts().then((body) => setProducts(body?.body));
-  }, []);
+    await product.getProducts(productsOffset).then((body) => setProducts(body?.body));
+  }, [productsOffset]);
+
+  const changePage = useCallback(
+    (event: React.MouseEvent) => {
+      if ((event.target as HTMLInputElement).className === 'next-page' && products?.total) {
+        if (productsOffset < products?.total - products.count) {
+          setPagin(productsOffset + 8);
+          setPage(page + 1);
+        }
+      } else if (
+        (event.target as HTMLInputElement).className === 'previous-page' &&
+        productsOffset > 0
+      ) {
+        setPagin(productsOffset - 8);
+        setPage(page - 1);
+      }
+    },
+    [productsOffset, products, page]
+  );
 
   useEffect(() => {
     getProducts();
-    showName && showName('Catalog Page');
   }, [getProducts]);
   return (
     <>
       <div className={classes.cardsContainer}>
         {products?.results.map((item) => {
           if (
-            item.masterData.current.masterVariant.prices &&
-            item.masterData.published &&
-            item.masterData.current.masterVariant.images &&
-            item.masterData.current.description
+            item.masterVariant.prices &&
+            item.published &&
+            item.masterVariant.images &&
+            item.description
           ) {
-            return (
-              <Card
-                uniqueKey={item.id}
-                id={item.id}
-                url={item.masterData.current.masterVariant.images[0].url}
-                title={item.masterData.current.name['ru-BY']}
-                price={item.masterData.current.masterVariant.prices[0].value.centAmount}
-                discounted={1}
-                description={item.masterData.current.description['ru-BY']}
-              />
-            );
+            if (item.masterVariant.prices[0].discounted) {
+              return (
+                <Card
+                  uniqueKey={item.id}
+                  id={item.id}
+                  url={item.masterVariant.images[0].url}
+                  title={item.name['ru-BY']}
+                  price={item.masterVariant.prices[0].value.centAmount}
+                  discounted={item.masterVariant.prices[0].discounted.value.centAmount}
+                  currency={item.masterVariant.prices[0].value.currencyCode}
+                  description={item.description['ru-BY']}
+                />
+              );
+            } else {
+              return (
+                <Card
+                  uniqueKey={item.id}
+                  id={item.id}
+                  url={item.masterVariant.images[0].url}
+                  title={item.name['ru-BY']}
+                  price={item.masterVariant.prices[0].value.centAmount}
+                  currency={item.masterVariant.prices[0].value.currencyCode}
+                  description={item.description['ru-BY']}
+                />
+              );
+            }
           }
         })}
       </div>
       <div className={classes.btncontainer}>
-        <Button>1</Button>
-        <Button>2</Button>
-        <Button>3</Button>
+        <input type="button" className="previous-page" value="<" onClick={changePage} />
+        <div>{page}</div>
+        <input type="button" className="next-page" value=">" onClick={changePage} />
       </div>
     </>
   );
