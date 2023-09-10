e5592185ba8d3fad50743222a212caced2044b11
Ruslanbek <you@example.com>
2023-09-10 13:25:44 +0500

    fix: fix deployment issues

--- src/components/CardButton.tsx
+++ src/components/CardButton.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 
-import classes from '../css/ui.module.css';
-import plus from '../assets/plus.svg';
 import checked from '../assets/checked.svg';
+import plus from '../assets/plus.svg';
+import classes from '../css/ui.module.css';
 
 export const CardButton = () => {
   const [isAdded, setAdding] = useState(false);
   return (
-    <a className={`${classes.button} ${classes.cardBtn}`}>
+    <a className={`${classes.button} ${classes.cardBtn}`} onClick={() => setAdding(!isAdded)}>
       <img src={isAdded ? checked : plus} alt="" />
       <span>{isAdded ? 'Added' : 'Add to Cart'}</span>
     </a>
